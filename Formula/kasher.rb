b32ff6a328aac62cfb50622bae56301f0a4dcb4c
Matthew Villeneuve <matthew.villeneuve@gmail.com>
2025-06-28 14:59:01 -0700

    beta 2

--- Formula/kasher.rb
+++ Formula/kasher.rb
@@ -1,9 +1,9 @@
 class Kasher < Formula
   desc "CLI for defining and caching shell commands"
   homepage "https://github.com/iammatthew2/kasher"
-  url "https://github.com/iammatthew2/kasher/releases/download/v0.1.0-beta.1/kasher_darwin_amd64.zip"
-  sha256 "e996ac112ff322642ffac544498e252daa255e7360dc5ed6fae7898d57d952f6"
-  version "0.1.0-beta.1"
+  url "https://github.com/iammatthew2/kasher/releases/download/v0.1.0-beta.2/kasher_darwin_amd64.zip"
+  sha256 "6329aca65248f1b01901a21552cea9309b09f3bd7808783f68bfc8a2a8f0f8b4"
+  version "0.1.0-beta.2"
 
   def install
     bin.install "kasher"
